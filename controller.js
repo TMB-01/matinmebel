5bcb1f70f194bb9ab96a6078b04ef847fdf7fffb
Maraim <maraimtuxtasunov@gmail.com>
2023-05-09 18:46:08 +0500

    increased limit to 50mb

--- controller.js
+++ controller.js
@@ -43,7 +43,9 @@ require("dotenv").config();
 
 
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors());
 const port = process.env.PORT
 app.use(authMiddleware())
